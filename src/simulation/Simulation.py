a1304d6d937a32d3f74c437755d307359ac7663a
Artem Hontarenko <artem.hontarenko.ua@gmail.com>
2026-01-24 01:12:50 +0100

    Add implementation of the Simulator

--- src/simulation/Simulation.py
+++ src/simulation/Simulation.py
@@ -32,22 +32,3 @@ class SignBasedStrategy(Simulation):
         strategy_simple_returns = signals * self.market_simple_returns
         
         return strategy_simple_returns.dropna()
-
-class ProportionalStrategy(Simulation):
-    """
-    Position size is proportional to the predicted return.
-    Sensitive to magnitude changes, making it ideal for Optimization.
-    """
-    def __init__(self, data_wrapper: DataWrapper, leverage_scale: float = 100.0):
-        super().__init__(data_wrapper)
-        self.leverage_scale = leverage_scale
-
-    def calculate_returns(self, predictions: pd.Series) -> pd.Series:
-        leverage = predictions * self.leverage_scale
-        
-        leverage = leverage.clip(-2.0, 2.0)
-        
-        aligned_leverage = leverage.reindex(self.market_simple_returns.index).fillna(0)
-        
-        strategy_simple_returns = aligned_leverage * self.market_simple_returns
-        return strategy_simple_returns.dropna()
